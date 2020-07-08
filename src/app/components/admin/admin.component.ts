34c6cf9921c6b009734572c18479fa2fd3597e75
AbdelkaderBahroun <abdelkader.bahroun@operam.fr>
2020-07-08 15:51:22 +0200

    edit praticien partie admin

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -15,8 +15,14 @@ praticienTmp = new Praticien('', '');
   }
 
     praticiens: Array<Praticien>;
+    praticienEdited: Praticien;
+    specialiteEdited: string;
+    locationEdited: string;
+    motifEdited: string;
+    showEdit: boolean = false;
 
-  ngOnInit(): void {
+
+    ngOnInit(): void {
   }
 
     load(): void {
@@ -31,8 +37,40 @@ praticienTmp = new Praticien('', '');
         this.http.delete('http://localhost:3000/praticiens/' + id).subscribe(() => this.praticienService.load(), error => console.log(error));
     }
 
-    editPraticien(praticien: Praticien): void {
-        this.http.put<Praticien>('http://localhost:3000/praticiens/' + praticien.id, praticien).subscribe(() => this.praticienService.load(), error => console.log(error));
+    editPraticien(praticien: Praticien) {
+        this.praticienEdited = new Praticien(praticien.prenom, praticien.nom);
+        this.praticienEdited.specialites = new Array<string>();
+        this.praticienEdited.consultLocations = new Array<string>();
+        this.praticienEdited.motifs = new Array<string>();
+        if (praticien.specialites) {
+            praticien.specialites.forEach(value => this.praticienEdited.specialites.push(value));
+        }
+        if (praticien.consultLocations) {
+            praticien.consultLocations.forEach(value => this.praticienEdited.consultLocations.push(value));
+        }
+        if (praticien.motifs) {
+            praticien.motifs.forEach(value => this.praticienEdited.motifs.push(value));
+        }
+        this.praticienEdited.id = praticien.id;
+        this.showEdit = true;
+    }
+
+    launchEdit() {
+        if (this.specialiteEdited) {
+            this.praticienEdited.specialites.push(this.specialiteEdited);
+        }
+        if (this.locationEdited) {
+            this.praticienEdited.consultLocations.push(this.locationEdited);
+        }
+        if (this.motifEdited) {
+            this.praticienEdited.motifs.push(this.motifEdited);
+        }
+        this.praticienService.updatePraticien(this.praticienEdited);
+        this.showEdit = false;
+        this.praticienEdited = new Praticien('', '', new Array<string>(), new Array<string>());
+        this.specialiteEdited = null;
+        this.locationEdited = null;
+        this.motifEdited = null;
     }
 
 

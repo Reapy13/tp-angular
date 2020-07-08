9ed29ae5f7e69f19dee364a653ed3350e6d3a4e9
AbdelkaderBahroun <abdelkader.bahroun@operam.fr>
2020-07-08 15:35:23 +0200

    list praticiens

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Praticien} from "../../model/praticien";
-import {PraticienService} from "../../services/praticien.service";
+import {Praticien} from '../../model/praticien';
+import {PraticienService} from '../../services/praticien.service';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-admin',
@@ -9,9 +11,30 @@ import {PraticienService} from "../../services/praticien.service";
 })
 export class AdminComponent implements OnInit {
 praticienTmp = new Praticien('', '');
-  constructor(private praticienService: PraticienService) {
+  constructor(private praticienService: PraticienService, private http: HttpClient) {
   }
+
+    praticiens: Array<Praticien>;
 
   ngOnInit(): void {
   }
+
+    load(): void {
+        this.http.get<Array<Praticien>>('http://localhost:3000/praticiens').subscribe(resp => this.praticiens = resp, error => console.log(error));
+    }
+
+    findAll(): Array<Praticien> {
+        return this.praticienService.getAllPraticiens();
+    }
+
+    deletePraticien(id: number): void {
+        this.http.delete('http://localhost:3000/praticiens/' + id).subscribe(() => this.praticienService.load(), error => console.log(error));
+    }
+
+    editPraticien(praticien: Praticien): void {
+        this.http.put<Praticien>('http://localhost:3000/praticiens/' + praticien.id, praticien).subscribe(() => this.praticienService.load(), error => console.log(error));
+    }
+
+
+
 }
